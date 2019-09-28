aad8697e8986d23fb62b5baad5968d640b12dc46
andreasonny83 <andreasonny83@gmail.com>
2019-09-28 13:51:26 +0100

    perf(useBasicFetch): Major refactoring

--- src/useBasicFetch.test.tsx
+++ src/useBasicFetch.test.tsx
@@ -1,31 +1,165 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { mount } from 'enzyme';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 
-import { useBasicFetch } from './index';
+import { useBasicFetch } from './useBasicFetch';
+
+jest.useFakeTimers();
+
+const checkRenderedComponent = (target: any) => {
+  if (!target) {
+    throw new Error('Cannot render component');
+  }
+
+  return target as HTMLElement;
+};
 
 describe('useBasicFetch', () => {
-  const TestComponent = () => {
-    const { data, loading, error } = useBasicFetch();
+  let container: HTMLElement | null;
+
+  const TestComponent = ({ url = '', delay = 0 }) => {
+    const { data, loading, error } = useBasicFetch(url, delay);
 
     if (error) {
-      return null;
+      return <p>Some Error Message</p>;
     }
-    if (loading || !data) {
+    if (loading) {
       return <p>loading...</p>;
     }
-    return <p>All ok!</p>;
+
+    return <div className="TestComponent">{data && <p>{data}</p>}</div>;
   };
 
-  it('should render without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TestComponent />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+    container = document.createElement('div');
+    document.body.appendChild(container);
   });
 
-  it('should load some data', async () => {
-    const wrapper = mount(<TestComponent />);
+  afterEach(() => {
+    if (container) {
+      document.body.removeChild(container);
+    }
 
-    expect(wrapper.find(TestComponent).text()).toBe('loading...');
+    container = null;
+  });
+
+  it('should render without crashing', () => {
+    act(() => {
+      ReactDOM.render(<TestComponent />, container);
+    });
+  });
+
+  it('should do nothing when an URL is not provided', () => {
+    act(() => {
+      ReactDOM.render(<TestComponent />, container);
+    });
+
+    const el = checkRenderedComponent(container);
+
+    expect(el.textContent).toBe('');
+  });
+
+  it('should start fetching some data after the component is mounted', () => {
+    const testUrl = 'fakeUrl';
+
+    act(() => {
+      ReactDOM.render(<TestComponent url={testUrl} delay={1} />, container);
+    });
+
+    const el = checkRenderedComponent(container);
+    const content = el.querySelector('p') as HTMLElement;
+
+    expect(content).toBeTruthy();
+    expect(content.textContent).toBe('loading...');
+  });
+
+  it('should return an error when the http request fails', async () => {
+    const testUrl = 'fakeUrl';
+
+    //@ts-ignore
+    global.fetch = jest.fn().mockImplementationOnce(() => {
+      throw new Error('Something went wrong');
+    });
+
+    await act(async () => {
+      await ReactDOM.render(<TestComponent url={testUrl} />, container);
+    });
+
+    const el = checkRenderedComponent(container);
+    const content = el.querySelector('p') as HTMLElement;
+
+    //@ts-ignore
+    expect(global.fetch).toHaveBeenCalledWith(testUrl);
+
+    expect(content).toBeTruthy();
+    expect(content.textContent).toBe('Some Error Message');
+  });
+
+  it('should initialize an http request', async () => {
+    const testUrl = 'fakeUrl';
+    const expectedData = 'Allo!';
+
+    //@ts-ignore
+    global.fetch = jest.fn().mockImplementationOnce(() =>
+      Promise.resolve({
+        json: () => expectedData,
+      })
+    );
+
+    await act(async () => {
+      await ReactDOM.render(<TestComponent url={testUrl} />, container);
+    });
+
+    const el = checkRenderedComponent(container);
+    const content = el.querySelector('p') as HTMLElement;
+
+    //@ts-ignore
+    expect(global.fetch).toHaveBeenCalledWith(testUrl);
+
+    expect(content).toBeTruthy();
+    expect(content.textContent).toBe(expectedData);
+  });
+
+  it('should delay the response when a "delay" is provided', async () => {
+    const testUrl = 'fakeUrl';
+    const testDelay = 1000;
+    const expectedData = 'Allo!';
+
+    //@ts-ignore
+    global.fetch = jest.fn().mockImplementationOnce(() =>
+      Promise.resolve({
+        json: () => expectedData,
+      })
+    );
+
+    await act(async () => {
+      await ReactDOM.render(
+        <TestComponent url={testUrl} delay={testDelay} />,
+        container
+      );
+    });
+
+    let el = checkRenderedComponent(container);
+    let content = el.querySelector('p') as HTMLElement;
+
+    //@ts-ignore
+    expect(global.fetch).not.toHaveBeenCalled();
+    expect(content).toBeTruthy();
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+    expect(content.textContent).toBe('loading...');
+
+    await act(async () => {
+      await jest.advanceTimersByTime(testDelay);
+    });
+
+    content = el.querySelector('p') as HTMLElement;
+
+    //@ts-ignore
+    expect(global.fetch).toHaveBeenCalledWith(testUrl);
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(content.textContent).toBe(expectedData);
   });
 });
---
+++ src/useBasicFetch.ts
@@ -0,0 +1,95 @@
+import { useCallback, useReducer, useEffect } from 'react';
+
+const myFetch = async (url: string, delay: number = 0) => {
+  if (delay) {
+    await new Promise(res => setTimeout(res, delay));
+  }
+
+  return fetch(url).then(res => res.json());
+};
+
+type Action = {
+  type: string;
+  payload?: any;
+};
+
+const dataFetchReducer = <T>() => (state: T, action: Action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        data: action.payload,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useBasicFetch = <Data>(url?: string, delay: number = 0) => {
+  type State = {
+    error: boolean;
+    loading: boolean;
+    data?: Data;
+  };
+
+  const initialState: State = {
+    error: false,
+    loading: false,
+    data: undefined,
+  };
+
+  const [state, dispatch] = useReducer(dataFetchReducer<State>(), initialState);
+
+  const setFetching = () => {
+    dispatch({ type: 'FETCH_INIT' });
+  };
+
+  const setError = () => {
+    dispatch({ type: 'FETCH_FAILURE' });
+  };
+
+  const setResponse = (data: any) => {
+    dispatch({ type: 'FETCH_SUCCESS', payload: data });
+  };
+
+  const fetchData = useCallback(async (url: string, delay: number = 0) => {
+    setFetching();
+
+    let res;
+    try {
+      res = await myFetch(url, delay);
+    } catch (err) {
+      setError();
+      return;
+    }
+
+    setResponse(res);
+  }, []);
+
+  useEffect(() => {
+    if (url) {
+      fetchData(url, delay);
+    }
+  }, [fetchData, url, delay]);
+
+  return {
+    data: state.data,
+    error: state.error,
+    loading: state.loading,
+    fetchData,
+  };
+};
